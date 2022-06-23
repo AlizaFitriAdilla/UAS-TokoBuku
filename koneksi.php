3d9cc81f4231c9e3f3df1a230ee7702eb99b2860
AlizaFitriAdilla <alizafitri08@gmail.com>
2022-06-23 12:15:51 +0700

    make it better

---
+++ koneksi.php
@@ -0,0 +1,14 @@
+<?php
+$conn_str = "host=heffalump.db.elephantsql.com" .
+    "port=5432 " .
+    "user=cpxuzmfn" .
+    "dbname=cpxuzmfn" .
+    "password=HeXjWc8OraOeVN9KgGIbo59bn7oRBbT6";
+$conn = pg_connect($conn_str);
+
+// if($conn) {
+//     echo "<h3>Koneksi Berhasil</h3>";
+// } else {
+//     echo "<h3>Koneksi GAGAL TERSAMBUNG</h3>";
+// }
+?>
